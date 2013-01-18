13648409a7f4f9c87b117a69a6789897b55de1ec
Daniel Beilinson <beilinson@gmail.com>
2013-01-18 19:58:14 +0400

    misc

--- ext/js/app.js
+++ ext/js/app.js
@@ -105,7 +105,7 @@ function buildTable(data) {
         	group.detentions[i] = [Date.parse(group[i].date,"dd.MM.yyyy"), parseInt(group[i].value)];
         	i++;
       	}
-      	//org = _.sortBy(org, function(obj){ return obj.total; });
+      	_.sortBy(group, function(obj){ return obj[0]; });
       	//output.name = name;
       	//output.total = sum;*/
       	//return output;
