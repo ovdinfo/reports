cc7de4c3e8177b60d0d1e2234c20010906c1770b
Daniel Beilinson <beilinson@gmail.com>
2013-01-19 02:59:40 +0400

    misc

--- ext/js/app.js
+++ ext/js/app.js
@@ -133,8 +133,7 @@ function drawVisualization(ovd) {
           vAxis: {title: 'Количество задержаний', textStyle: {color: 'white'}, titleTextStyle: {color: 'white'}, baselineColor: 'white'},
           legend: 'none',
           backgroundColor: { fill:'transparent' },
-          colors: ['orange', '#ff6600'],
-          series: {12:{color: 'black', visibleInLegend: false}, 3:{color: 'red', visibleInLegend: false}}
+          colors: ['#F77807'],
         };
       
         // Create and draw the visualization.
