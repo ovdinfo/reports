5548ffae4182dd4563035b71871fb9e1e0be3aa3
Daniel Beilinson <beilinson@gmail.com>
2013-01-20 03:03:46 +0400

    misc

--- ext/js/app.js
+++ ext/js/app.js
@@ -1,14 +1,14 @@
 var opts = {
   lines: 13, // The number of lines to draw
-  length: 7, // The length of each line
-  width: 4, // The line thickness
-  radius: 10, // The radius of the inner circle
+  length: 13, // The length of each line
+  width: 8, // The line thickness
+  radius: 29, // The radius of the inner circle
   corners: 1, // Corner roundness (0..1)
   rotate: 0, // The rotation offset
-  color: '#000', // #rgb or #rrggbb
+  color: '#fff', // #rgb or #rrggbb
   speed: 1, // Rounds per second
-  trail: 60, // Afterglow percentage
-  shadow: false, // Whether to render a shadow
+  trail: 58, // Afterglow percentage
+  shadow: true, // Whether to render a shadow
   hwaccel: false, // Whether to use hardware acceleration
   className: 'spinner', // The CSS class to assign to the spinner
   zIndex: 2e9, // The z-index (defaults to 2000000000)
