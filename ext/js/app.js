2e3eb9b6278c5da37f3abd874fcd807f90ef13b0
Daniel Beilinson <beilinson@gmail.com>
2013-01-21 02:06:24 +0400

    misc

--- ext/js/app.js
+++ ext/js/app.js
@@ -131,7 +131,14 @@ function buildTable(data) {
     $('#table-wrapper table tbody').append(content);
   });
   $.tablesorter.defaults.widgets = ['zebra'];
-  $('#table-wrapper table').tablesorter({dateFormat : "mm.dd.yyyy"});
+  $('#table-wrapper table').tablesorter({
+  	dateFormat : "mm.dd.yyyy",
+  	headers: { 
+      0: { sorter: "shortDate" } //, dateFormat will parsed as the default above 
+      // 1: { sorter: "shortDate", dateFormat: "ddmmyyyy" }, // set day first format; set using class names 
+      // 2: { sorter: "shortDate", dateFormat: "yyyymmdd" }  // set year first format; set using data attributes (jQuery data) 
+    } 
+  });
 };
 
 function drawVisualization(ovd) {
