aaacf12f12d7130db9ce87f154a2ff37951318b3
Daniel Beilinson <beilinson@gmail.com>
2013-01-22 00:13:54 +0400

    misc

--- ext/js/data.js
+++ ext/js/data.js
@@ -54,23 +54,6 @@ function buildTable(data) {
  
     } 
   }) 
-  .tablesorterPager({ 
- 
-    // target the pager markup - see the HTML block below 
-    container: $(".pager"), 
- 
-    // target the pager page select dropdown - choose a page 
-    cssGoto  : ".pagenum", 
- 
-    // remove rows from the table to speed up the sort of large tables. 
-    // setting this to false, only hides the non-visible rows; needed if you plan to add/remove rows with the pager enabled. 
-    removeRows: false, 
- 
-    // output string - default is '{page}/{totalPages}'; 
-    // possible variables: {page}, {totalPages}, {filteredPages}, {startRow}, {endRow}, {filteredRows} and {totalRows} 
-    output: '{startRow} - {endRow} / {filteredRows} ({totalRows})' 
- 
-  }); 
  
 });
   $('#table-wrapper table').show('slow');
