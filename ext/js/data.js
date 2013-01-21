a53e6e1cd54839b6d97527f85c44331a3a3ab81c
Daniel Beilinson <beilinson@gmail.com>
2013-01-22 01:24:05 +0400

    misc

--- ext/js/data.js
+++ ext/js/data.js
@@ -10,7 +10,12 @@ function buildTable(data) {
         datalinks = val.gsx$links.$t.split(', '),
         i = 0;
     while (i < datalinks.length) {
-      links += '<a href=' + datalinks[i] + '>источник ' + (i + 1) + '</a>';
+      if (i + 1 != datalinks.length) {
+        links += '<a href=' + datalinks[i] + '>' + (i + 1) + ', </a>';
+      }
+      else {
+        links += '<a href=' + datalinks[i] + '>' + (i + 1) + '</a>';
+      }
       i++;
     }
     var content = '<tr class="data ' + val.gsx$id.$t + '"><td><a id="' + val.gsx$id.$t +'"></a>' + val.gsx$date.$t + '</td><td>' + val.gsx$agreement.$t + '</td><td>' + val.gsx$eventtype.$t + '</td><td>' + val.gsx$subject.$t + '</td><td>' + val.gsx$organizer.$t + '</td><td>' + val.gsx$description.$t + '</td><td>' + val.gsx$numberofdetentions.$t + '</td><td>' + links + '</td></tr>';
@@ -40,7 +45,7 @@ function buildTable(data) {
     },
     theme : "bootstrap", // this will  
  
-    widthFixed: true, 
+    widthFixed: false, 
  
     headerTemplate : '{content} {icon}', // new in v2.7. Needed to add the bootstrap icon! 
  
