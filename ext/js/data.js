152ffac616da5973ff042ab7cf0bde669c39bf73
Daniel Beilinson <beilinson@gmail.com>
2013-01-21 16:57:50 +0400

    misc

--- ext/js/data.js
+++ ext/js/data.js
@@ -6,7 +6,7 @@ $.ajax({
 
 function buildTable(data) {
   $.each(data.feed.entry, function (key, val) {
-    var content = '<tr class="data ' + val.gsx$id.$t + ' hider"><td>' + val.gsx$date.$t + '</td><td>' + val.gsx$agreement.$t + '</td><td>' + val.gsx$eventtype.$t + '</td><td>' + val.gsx$subject.$t + '</td><td>' + val.gsx$organizer.$t + '</td><td>' + val.gsx$description.$t + '</td><td>' + val.gsx$numberofdetentions.$t + '</td><td>' + val.gsx$links.$t + '</td></tr>';
+    var content = '<tr class="data ' + val.gsx$id.$t + '"><td>' + val.gsx$date.$t + '</td><td>' + val.gsx$agreement.$t + '</td><td>' + val.gsx$eventtype.$t + '</td><td>' + val.gsx$subject.$t + '</td><td>' + val.gsx$organizer.$t + '</td><td>' + val.gsx$description.$t + '</td><td>' + val.gsx$numberofdetentions.$t + '</td><td>' + val.gsx$links.$t + '</td></tr>';
     $('#table-wrapper table tbody').append(content);
   });
   $.tablesorter.defaults.widgets = ['zebra'];
@@ -17,5 +17,8 @@ function buildTable(data) {
       // 1: { sorter: "shortDate", dateFormat: "ddmmyyyy" }, // set day first format; set using class names 
       // 2: { sorter: "shortDate", dateFormat: "yyyymmdd" }  // set year first format; set using data attributes (jQuery data) 
     } 
+  });
+  $('#table-wrapper table').toggle('slow', function() {
+    $(this).toggleClass('hider');
   });
 };
