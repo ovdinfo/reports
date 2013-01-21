3f296d551a7387762b4429ec12833db70a152929
Daniel Beilinson <beilinson@gmail.com>
2013-01-22 00:16:58 +0400

    misc

--- ext/js/data.js
+++ ext/js/data.js
@@ -9,6 +9,7 @@ function buildTable(data) {
     var content = '<tr class="data ' + val.gsx$id.$t + '"><td><a id="' + val.gsx$id.$t +'"></a>' + val.gsx$date.$t + '</td><td>' + val.gsx$agreement.$t + '</td><td>' + val.gsx$eventtype.$t + '</td><td>' + val.gsx$subject.$t + '</td><td>' + val.gsx$organizer.$t + '</td><td>' + val.gsx$description.$t + '</td><td>' + val.gsx$numberofdetentions.$t + '</td><td>' + val.gsx$links.$t + '</td></tr>';
     $('#table-wrapper table tbody').append(content);
   });
+  $(function() {
   $.extend($.tablesorter.themes.bootstrap, { 
     // these classes are added to the table. To see other table classes available, 
     // look here: http://twitter.github.com/bootstrap/base-css.html#tables 
@@ -51,6 +52,7 @@ function buildTable(data) {
  
       // set the uitheme widget to use the bootstrap theme class names 
       // uitheme : "bootstrap" 
+  });
   });
   $('#table-wrapper table').show('slow');
   if(window.location.hash) {
