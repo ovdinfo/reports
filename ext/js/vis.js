bc477e3f54a166020f41c4f518bb30235913779d
Daniel Beilinson <beilinson@gmail.com>
2013-01-23 18:42:17 +0400

    misc

--- ext/js/vis.js
+++ ext/js/vis.js
@@ -312,10 +312,8 @@ BubbleChart = (function() {
     var _this = this;
     this.force.gravity(this.layout_gravity).charge(this.charge).friction(0.9).on("tick", function(e) {
       return _this.circles.each(_this.move_towards_date(e.alpha)).transition().duration(100).ease("easing").attr("cx", function(d) {
-        //alert(d.x);
         return d.x;
       }).attr("cy", function(d) {
-        //alert(d.y);
         return d.y;
       });
     });
@@ -324,8 +322,6 @@ BubbleChart = (function() {
     this.hide_years();
     this.hide_orgs();
     $("#det-slider").bind("valuesChanged", function(e, data){ 
-      console.log(data);
-      //return;
       _this.update();
       $( "#count" ).empty().append( "задержания от " + Math.round(data.values.min) +  " до " + Math.round(data.values.max) + " человек" );
     });
