acbc44402c471c2f1dee64957203291e2471bc36
Daniel Beilinson <beilinson@gmail.com>
2013-01-23 23:56:03 +0400

    misc

--- ext/libs/d3/d3.legend.js
+++ ext/libs/d3/d3.legend.js
@@ -30,7 +30,7 @@ d3.legend = function(g) {
         .data(items,function(d) { return d.key})
         .call(function(d) { d.enter().append("text")})
         .call(function(d) { d.exit().remove()})
-        .attr("y",function(d,i) { return (i*20+10)})
+        .attr("y",function(d,i) { return (i*20+13)})
         .attr("x","3em")
         .text(function(d) { ;return d.key})
     
