3477c6aeaba82a83a0311d5327ad81dc2ef0d5d0
Daniel Beilinson <beilinson@gmail.com>
2013-01-23 22:39:31 +0400

    misc

--- ext/js/CustomTooltip.js
+++ ext/js/CustomTooltip.js
@@ -24,7 +24,7 @@ function CustomTooltip(tooltipId, width){
 	    var cy = $(obj).attr("cy");
 	    var r = $(obj).attr("r");
 	    r = Math.round(r);
-	    var tttop = 200 - r;
+	    var tttop = 170 - r;
 	    cx = Math.round(cx) + ttleft;
 	    cy = Math.round(cy) + tttop;
 		var ttid = "#"+tooltipId;
