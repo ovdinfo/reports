286930026997803099f05a20e58b41883fefe5af
Daniel Beilinson <beilinson@gmail.com>
2013-01-26 02:44:54 +0400

    misc

--- ext/js/vis.js
+++ ext/js/vis.js
@@ -5,7 +5,7 @@ BubbleChart = (function() {
   var B, J, xScale, yScale;
 
   function BubbleChart(data) {
-    $('#loader').remove();
+    //$('#loader').remove();
 
     var i, max_amount, organizators;
     this.data = data;
