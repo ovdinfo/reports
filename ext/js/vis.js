7897e624610f587aca9fff8aa080f7d5336686ee
behlbor <behlbor@gmail.com>
2013-01-27 01:43:40 +0400

    Update ext/js/vis.js

--- ext/js/vis.js
+++ ext/js/vis.js
@@ -647,16 +647,16 @@ BubbleChart = (function() {
   //0 - 
    console.log('old: ' + this.state);
    console.log('new: ' + newState);
-   switch(this.state){
-   	case 0: this.hide_label();
-   	case 1: this.hide_agrs();
-   	case 2: this.hide_axis();
-   	case 3: this.hide_orgs();
+   var oldState = this.state;
+   this.state = newState;
+   switch(oldState){
+   	case 0: return this.hide_label();
+   	case 1: return this.hide_agrs();
+   	case 2: return this.hide_axis();
+   	case 3: return this.hide_orgs();
    	//case 4: true;
    	default: ;
    };
-   this.state = newState;
-   return this.force.start();
   };
 
   
