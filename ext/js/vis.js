e9df537482019603022583c276ef0ff25d8edf8c
Daniel Beilinson <beilinson@gmail.com>
2013-01-27 03:16:00 +0400

    misc

--- ext/js/vis.js
+++ ext/js/vis.js
@@ -646,7 +646,7 @@ BubbleChart = (function() {
     return function(d) {
       var x, y;
       x = (d.inTypeID%4)*200+200;
-      y = parseInt(d.inTypeID/4)*200+200;
+      y = parseInt(d.inTypeID/4)*100+200;
       d.x = d.x + (x - d.x) * (_this.damper + 0.02) * alpha * 1.1;
       return d.y = d.y + (y - d.y) * (_this.damper + 0.02) * alpha * 1.1;
     };
