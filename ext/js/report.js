08bea5e15aac8416bc9505e477b7b07d67737be7
Daniel Beilinson <beilinson@gmail.com>
2013-01-31 12:15:01 +0400

    misc

--- ext/js/report.js
+++ ext/js/report.js
@@ -4,7 +4,7 @@ requirejs.config({
     paths: { 
         /* Load jquery from google cdn. On fail, load local file. */
         'jquery': ['//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min','libs/jquery/jquery.min'],
-        'jquery-ui': ['//http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min','libs/jquery-ui/jquery-ui.min'],
+        'jquery-ui': ['//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min','libs/jquery-ui/jquery-ui.min'],
         /* Load bootstrap from cdn. On fail, load local file. */
         'bootstrap': ['//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min','libs/bootstrap/js/bootstrap.min'],
         'tocify': 'lib/tocify/jquery.tocify.min'
