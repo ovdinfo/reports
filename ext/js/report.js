106bf54c3753bfff53001c8bd36fe2cb211553aa
Daniel Beilinson <beilinson@gmail.com>
2013-01-31 12:10:31 +0400

    misc

---
+++ ext/js/report.js
@@ -0,0 +1,26 @@
+requirejs.config({
+    appDir: ".",
+    baseUrl: "/ext",
+    paths: { 
+        /* Load jquery from google cdn. On fail, load local file. */
+        'jquery': ['//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min','libs/jquery/jquery.min'],
+        'jquery-ui': ['//http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min','libs/jquery-ui/jquery-ui.min'],
+        /* Load bootstrap from cdn. On fail, load local file. */
+        'bootstrap': ['//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min','libs/bootstrap/js/bootstrap.min'],
+        'tocify': 'lib/tocify/jquery.tocify.min'
+    },
+    shim: {
+        /* Set bootstrap dependencies (just jQuery) */
+        'bootstrap' : ['jquery'],
+        'tocify' : ['jquery','jquery-ui','bootstrap']
+    }
+});
+
+require([
+    'jquery', 'jquery-ui', 'bootstrap',
+],
+function($){
+    $("#toc").tocify();
+    console.log("Loaded :)");    
+    return {};
+});
