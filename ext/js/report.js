da3f7e87a8ec483402121c9b00a328a5c997fb83
Daniel Beilinson <beilinson@gmail.com>
2013-01-31 20:06:15 +0400

    misc

--- ext/js/report.js
+++ ext/js/report.js
@@ -2,7 +2,7 @@ requirejs.config({
     appDir: ".",
     baseUrl: "/ext",
     paths: { 
-        'jquery': ['//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min','libs/jquery/jquery.min'],
+        'jQuery': ['//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min','libs/jquery/jquery.min'],
         'jquery-ui': ['//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min','libs/jquery-ui/jquery-ui.min'],
         'bootstrap': ['//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min','libs/bootstrap/js/bootstrap.min'],
         'tocify': 'libs/tocify/jquery.tocify.min'
@@ -13,9 +13,9 @@ requirejs.config({
 });
 
 require([
-    'jquery', 'jquery-ui', 'bootstrap', 'tocify'
+    'jQuery', 'jquery-ui', 'bootstrap', 'tocify'
 ],
-function($){
+function($,jQuery){
     $(function() {
     	$("#toc").tocify({context:'#app',selectors:'h2,h3'});
     });
