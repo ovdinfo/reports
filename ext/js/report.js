58399a888bc9cc233a18a3580627ad773cb6b799
Daniel Beilinson <beilinson@gmail.com>
2013-02-03 19:48:30 +0400

    misc

--- ext/js/report.js
+++ ext/js/report.js
@@ -48,26 +48,9 @@ function($,tablesorter){
     $('.event-format table tbody').append(content);
     });
   };
-  $.extend($.tablesorter.themes.bootstrap, { 
-    // these classes are added to the table. To see other table classes available, 
-    // look here: http://twitter.github.com/bootstrap/base-css.html#tables 
-    table      : 'table table-bordered', 
-    header     : 'bootstrap-header', // give the header a gradient background 
-    footerRow  : '', 
-    footerCells: '', 
-    icons      : '', // add "icon-white" to make them white; this icon class is added to the <i> in the header 
-    sortNone   : 'bootstrap-icon-unsorted', 
-    sortAsc    : 'icon-chevron-up', 
-    sortDesc   : 'icon-chevron-down', 
-    active     : '', // applied when column is sorted 
-    hover      : '', // use custom css here - bootstrap class may not override it 
-    filterRow  : '', // filter row class 
-    even       : '', // odd row zebra striping 
-    odd        : ''  // even row zebra striping 
-  });
+  
   $('.event-format table').tablesorter({
   	
-    theme : "bootstrap", // this will  
  
     widthFixed: false, 
  
@@ -75,7 +58,7 @@ function($,tablesorter){
  
     // widget code contained in the jquery.tablesorter.widgets.js file 
     // use the zebra stripe widget if you plan on hiding any rows (filter widget) 
-    widgets : [ "uitheme", "filter", "zebra" ], 
+    widgets : ["filter", "zebra" ], 
  
     widgetOptions : { 
       // using the default zebra striping class name, so it actually isn't included in the theme variable above 
