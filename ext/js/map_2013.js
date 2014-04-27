0a0d505febf26737e6d1c2f3cd1d65b213edcd71
Integral <beilinson@gmail.com>
2014-04-28 01:22:27 +0400

    misc

--- ext/js/map_2013.js
+++ ext/js/map_2013.js
@@ -12,7 +12,7 @@ requirejs.config({
         'mapbox': ['//api.tiles.mapbox.com/mapbox.js/v1.6.2/mapbox','libs/mapbox/latest/mapbox'],
         'Leaflet.fullscreen': 'libs/mapbox/Leaflet.fullscreen.min',
         'leaflet-heat': 'libs/mapbox/leaflet-heat',
-        'omnivore': ['//api.tiles.mapbox.com/mapbox.js/plugins/leaflet-omnivore/v0.0.1/leaflet-omnivore.min','libs/mapbox/leaflet-omnivore.min'],
+        'leaflet-omnivore': ['//api.tiles.mapbox.com/mapbox.js/plugins/leaflet-omnivore/v0.0.1/leaflet-omnivore.min','libs/mapbox/leaflet-omnivore.min'],
         'async': 'libs/require/async',
         'goog': 'libs/require/goog',
         'propertyParser': 'libs/require/propertyParser'
@@ -25,14 +25,17 @@ requirejs.config({
         'spin' : ['jquery'],
         'Leaflet.fullscreen': ['mapbox'],
         'leaflet-heat': ['mapbox'],
-        //'leaflet-omnivore': ['mapbox'],
+        'leaflet-omnivore': {
+          deps:['mapbox'], 
+          exports: 'omnivore'
+        }
     }
 });
 
 require([
-    'jquery', 'bootstrap', 'tablesorter', 'tablesorter.widgets', 'underscore', 'spin', 'leaflet-heat', 'moment', 'mapbox', 'Leaflet.fullscreen', 'omnivore', 'goog!visualization,1,packages:[corechart],language:ru'
+    'jquery', 'bootstrap', 'tablesorter', 'tablesorter.widgets', 'underscore', 'spin', 'leaflet-heat', 'moment', 'mapbox', 'Leaflet.fullscreen', 'leaflet-omnivore', 'goog!visualization,1,packages:[corechart],language:ru'
 ],
-function($,tablesorter,omnivore){
+function($,tablesorter){
   console.log(omnivore)
 $('#header ul.nav a[href="'+ window.location.pathname +'"]').parent().addClass('active');
 loadCss('/ext/libs/mapbox/latest/mapbox.css');
